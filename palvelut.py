16962f5dac3231934f5f1cf0fc151078e6a96ad4
Finnbyte <eelpy@protonmail.com>
2023-04-08 01:08:18 +0300

    Add Asiakas attributes

--- palvelut.py
+++ palvelut.py
@@ -1,8 +1,12 @@
-class Palvelu:
-    pass
-
 class Asiakas:
+    def __init__(self, nimi, ika):
+        self.__nimi = nimi
+        self.__ika = ika
+
+class Palvelu:
     pass
 
 class ParempiPalvelu:
     pass
+
+

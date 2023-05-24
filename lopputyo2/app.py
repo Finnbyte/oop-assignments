b795aa12a5537d82246aef3765b187696e8b809c
Finnbyte <eelpy@protonmail.com>
2023-05-24 14:52:50 +0300

    Add working modification of user data

--- lopputyo2/app.py
+++ lopputyo2/app.py
@@ -1,18 +1,27 @@
+from tkinter.constants import DISABLED, NORMAL
 from users import *
 from custom_types import Gender, UserExistsException
 import tkinter as tk
 from tkinter import messagebox
 
 class MyDialog:
-    def __init__(self, parent, labels):
+    def __init__(self, parent, labels, placeholders=None, title=None):
         top = self.top = tk.Toplevel(parent)
+        top.geometry("400x200")
+
+        if title is not None:
+            top.title(title)
+
         self.entries = []
+        self.exited_succesfully = False
         self.answers = []
-        for label in labels:
+        for i, label in enumerate(labels):
             label = tk.Label(top, text=label)
             label.pack()
 
             entry = tk.Entry(top)
+            if placeholders is not None and len(placeholders) >= i:
+                entry.insert(0, placeholders[i])
             self.entries.append(entry)
             entry.pack()
 
@@ -20,6 +29,7 @@ class MyDialog:
         self.mySubmitButton.pack()
 
     def _send(self):
+        self.exited_succesfully = True
         self.answers = [entry.get().strip() for entry in self.entries]
         self.top.destroy()
 
@@ -31,13 +41,17 @@ class UserManagementPanel:
         self.user_manager = UserManager()
         self.users = self.user_manager.fetch_users()
 
+        self.selected_idx = None
+
         self.create_widgets()
 
     def on_select(self, event):
-        selected_index = self.user_list.curselection()
-        if selected_index:
-            user = self.users[selected_index[0]]
+        if self.user_list.curselection():
+            self.selected_idx = self.user_list.curselection()
+            user = self.users[self.selected_idx[0]]
             self.display_user_data(user)
+        else:
+            self.selected_idx = None
 
     def create_widgets(self):
         # User list
@@ -64,8 +78,6 @@ class UserManagementPanel:
 
         self.wealth_label = tk.Label(self.details_frame, text="Varat:")
         self.wealth_label.pack(anchor=tk.W)
-        # Add user entry
-        self.add_entry = tk.Entry(self.root, width=30)
 
         # Buttons
         button_frame = tk.Frame(self.root)
@@ -87,27 +99,41 @@ class UserManagementPanel:
         self.wealth_label.config(text=f"Varat: {user.wealth or ''}")
        
     def add_user(self):
-        inputDialog = MyDialog(root, ("Nimi:", "Sukupuoli:", "Ikä:", "Varat:"))
+        inputDialog = MyDialog(root, ("Nimi:", "Sukupuoli:", "Ikä:", "Varat:"), "Lisää käyttäjä")
         root.wait_window(inputDialog.top)
-        data = inputDialog.answers
-
-        user = User(data[0], data[1], data[2], data[3])
-        if self.user_manager.add_user(user) is not UserExistsException:
-            self.users.append(user)
-            self.user_list.insert(tk.END, user.name)
+        if inputDialog.exited_succesfully:
+            data = inputDialog.answers
+            user = User(data[0], data[1], data[2], data[3])
+            if self.user_manager.add_user(user) is not UserExistsException:
+                self.users.append(user)
+                self.user_list.insert(tk.END, user.name)
 
     def modify_user(self):
         selected_index = self.user_list.curselection()
         if selected_index:
-            username = self.add_entry.get().strip()
-            if username:
-                self.user_list.delete(selected_index)
-                self.user_list.insert(selected_index, username)
-                self.add_entry.delete(0, tk.END)
-            else:
-                messagebox.showerror("Error", "Please enter a username.")
-        else:
-            messagebox.showerror("Error", "Please select a user.")
+            user = self.users[selected_index[0]]
+
+            inputDialog = MyDialog(root, 
+                                   placeholders=(user.name, user.gender, user.age, user.wealth), 
+                                   labels=("Uusi nimi:", "Uusi sukupuoli:", "Uusi ikä:", "Uudet varat:"), 
+                                   title=f"Muokkaa käyttäjää {user.name}")
+            root.wait_window(inputDialog.top)
+            if inputDialog.exited_succesfully:
+                data = inputDialog.answers                                                                              
+                try:
+                    self.user_manager.modify_user(user, data)
+
+                    updated_info_user = User(data[0], data[1], data[2], data[3])
+
+                    # Get index of name
+                    idx = self.user_list.get(0, tk.END).index(user.name)
+
+                    self.users[idx] = updated_info_user
+
+                    self.user_list.delete(idx)
+                    self.user_list.insert(idx, updated_info_user.name)
+                except Exception as e: 
+                    print("Error occured:", e)
 
     def delete_user(self):
         selected_index = self.user_list.curselection()
@@ -126,7 +152,7 @@ class UserManagementPanel:
                     self.user_list.delete(idx)
                 except Exception as e:
                     # Handle errors here
-                    print("Vittu")
+                    print("Error occured:", e)
         else:
             messagebox.showerror("Error", "Please select a user.")
 
